52b3614b09d2991ab69c770d15b5511a27f1d577
Nida <nidaslife23@gmail.com>
2019-12-20 21:21:51 +0530

    Cloud function register updated

--- YourSaathi/cloud/shared/user/models/user.model.js
+++ YourSaathi/cloud/shared/user/models/user.model.js
@@ -37,8 +37,39 @@ const validatePostData = async (params) => {
     }
     return { error: false, msg: null }
 }
-
-
+/**
+ * Get formatted User object
+ * @param {Object} user user object
+ * @return {Object} userObject
+ */
+const getUserObject = async (user) => {
+    let userObject = {};
+    let userImage = await user.get(fieldMapper.profileImage);
+    userObject.username = sharedHelper.getFieldValue(user, fieldMapper.username);
+    userObject.email = sharedHelper.getFieldValue(user, fieldMapper.email);
+    userObject.password = null;
+    userObject.profileImage = userImage ? userImage.url() : null;
+    userObject.userObjectId = user.id ? user.id : null;
+    userObject.name = sharedHelper.getFieldValue(user, fieldMapper.name);
+    userObject.birthDate = sharedHelper.getFieldValue(user, fieldMapper.birthDate);
+    userObject.gender = sharedHelper.getFieldValue(user, fieldMapper.gender);
+    userObject.mobileNumber = sharedHelper.getFieldValue(user, fieldMapper.mobileNumber);
+    return userObject;
+}
+/**
+ * Get User by Email Address
+ * @param {String} email email address of user
+ * @return {Object}
+ */
+const getUser = async (email) => {
+    let userData, user = null;
+    user = await sharedHelper.getDataByField(Parse.User, fieldMapper.username, email);
+    if (user)
+        userData = user;
+    else
+        userData = await sharedHelper.getDataByField(Parse.User, fieldMapper.email, email);
+    return userData;
+}
 /**
  * user Login
  * @param {Object} params parameters of user save
@@ -82,14 +113,6 @@ const logout = async () => {
     return isLogout;
 }
 
-
-
-
-
-
-
-
-
 /**
  * Validate device token
  * @param {String} deviceToken 
@@ -110,7 +133,39 @@ const isDeviceTokenExist = async (deviceToken) => {
         throw (err)
     }
 }
-
+/**
+ * SAVE USER
+ * @param{Object} all fields of user
+ * @return {Object} user
+ */
+const save = async (data) => {
+    let isRegistered = false;
+    let user = new Parse.User();
+    let { name, email, password, profileImage, birthDate, gender, mobileNumber } = data;
+    email = email.replace(/\s+/g, '');
+    try {
+        user.set(fieldMapper.username, email);
+        user.set(fieldMapper.name, name);
+        user.set(fieldMapper.password, password);
+        user.set(fieldMapper.email, email);
+        user.set(fieldMapper.gender, gender);
+        user.set(fieldMapper.mobileNumber, parseInt(mobileNumber));
+        if (birthDate instanceof Date)
+            user.set(fieldMapper.birthDate, birthDate);
+        if (profileImage) {
+            //Save profile image i.e. base64 String
+            let profileImageName = await saveProfileImage(profileImage);
+            user.set(fieldMapper.profileImage, profileImageName);
+        }
+        let NewUser = await user.signUp();
+        if (NewUser)
+            isRegistered = true;
+        return isRegistered;
+    } catch (error) {
+        console.log(error.message);
+        throw error;
+    }
+}
 
 module.exports = {
     
@@ -118,5 +173,8 @@ module.exports = {
     isAuthToken,
     login,
     logout,  
+    validatePostData,
+    getUser,
+    save,
     isDeviceTokenExist
 }
